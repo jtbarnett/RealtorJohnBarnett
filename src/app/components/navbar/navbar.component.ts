fc991125b77d0283a4e514dc0c0cf3dce8d9d95e
Jacob Barnett <jacob.barnett0417@gmail.com>
2020-07-17 18:25:14 -0400

    Added responsiveness to contact page, footer, header, and pre-payment page of rental application as well as changed the dashboard page style

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,9 +10,28 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
   }
 
   navProperties() {
     window.open('https://www.zillow.com/profile/JOHN-BARNETT/', '_blank');
+  }
+
+  /* When the user clicks on the button,
+  toggle between hiding and showing the dropdown content */
+  myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   }
 }
